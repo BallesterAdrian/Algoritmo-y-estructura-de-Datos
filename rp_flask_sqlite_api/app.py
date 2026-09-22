9f938843ea1d9d957519c12f0abcf59985ff068e
BallesterAdrian <francoballester06@gmail.com>
2026-09-22 20:15:41 -0300

    Delete rp_flask_sqlite_api directory

--- rp_flask_sqlite_api/app.py
+++
@@ -1,7 +0,0 @@
-from config import connex_app
-
-# Le indicamos el archivo de especificación Swagger
-connex_app.add_api("swagger.yml")
-
-if __name__ == "__main__":
-    connex_app.run(port=8000, debug=True)
